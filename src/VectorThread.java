0872855b2958605e8abd2a4dd972190c155c6314
EmilioPiedra <espiedra1@utpl.edu.ec>
2023-05-17 17:56:49 -0500

    Commit 02

---
+++ src/VectorThread.java
@@ -0,0 +1,48 @@
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class VectorThread extends Thread {
+    public int[] vector;
+
+    public VectorThread(int[] vector) {
+        this.vector = vector;
+    }
+
+    public void run() {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 100000) + 1;
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int tamañoVector =10000;
+
+        int[] vector1 = new int[tamañoVector];
+        int[] vector2 = new int[tamañoVector];
+        int[] vector3 = new int[tamañoVector];
+        int[] vector4 = new int[tamañoVector];
+        int[][] matriz = new int[4][tamañoVector]
+        VectorThread hilo1 = new VectorThread(vector1);
+        VectorThread hilo2 = new VectorThread(vector2);
+        VectorThread hilo3 = new VectorThread(vector3);
+        VectorThread hilo4 = new VectorThread(vector4);
+        hilo1.start();
+        Thread.sleep(1000);
+        hilo2.start();
+        Thread.sleep(1000);
+        hilo3.start();
+        Thread.sleep(1000);
+        hilo4.start();
+
+
+// Imprimir los vectores
+        System.out.println("Vector 1: " + Arrays.toString(vector1));
+        System.out.println("Vector 2: " + Arrays.toString(vector2));
+        System.out.println("Vector 3: " + Arrays.toString(vector3));
+        System.out.println("Vector 4: " + Arrays.toString(vector4));
+    }
+}
